28a6d9148f104bccf7c3482c8f3045ca2b19ffae
tjmhrdika <thomasjuanmahardika@gmail.com>
2022-12-13 22:29:00 +0700

    VS 2P Configuration

---
+++ PingPongGame_Kelompok9/src/application/Main.java
@@ -0,0 +1,22 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import view.GameView;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			GameView game = new GameView();
+			game.vs2pStart(primaryStage);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
